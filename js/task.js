3eb5751d52587122d3bb62af63a7799fec9c63ef
Marin <barisic.m@outlook.com>
2018-01-22 15:07:35 +0100

    scroll

--- js/task.js
+++ js/task.js
@@ -1,5 +1,8 @@
 function initialize() {
-    fetchAllUsers();
+
+    generateAndInsertPopupHtml($("#preloader"));
+
+    fetchUsersAndGenerateHtml(0);
 
     var popupActive=false;
     $(".popup-header-container").click(function(){
@@ -12,24 +15,76 @@ function initialize() {
         }
     });
 
-
     $(".user-selected-js").click(function(){
 
     });
-}
 
-function fetchAllUsers(){
-    $.ajax({
-        url: "https://api.github.com/users",
-        method: "GET",
-        success: function(data, status, jqXHR){
-            console.log(data);
-        }
+    var popupList = $('.users-list');
+    popupList.scroll(function () {
+
+        console.log($('.user-selected-js').height() * $('.user-selected-js').length);
+        console.log($('.users-list').scrollTop());
+        console.log($('.users-list').outerHeight());
+
+
+
+
+        // if (popupList.scrollTop() > 550) {
+        //     //TODO:: fetch last user and his index, and call fetch Users
+        //     var lastUser = $('.user-selected-js').last().prop("id");
+        //     fetchUsersAndGenerateHtml(lastUser);
+        // }
     });
 }
 
-function fetchedUsersToList(data, status){
-    
+function generateAndInsertPopupHtml(targetElement){
+    $(targetElement).after(
+    `<div id="users" class="row-full-width">
+        <div class="github-users">
+            <div class="popup-header-container">
+                <h3>GitHub Users</h3>
+            </div>
+            <div class="popup-list-container">
+                <ul class="users-list">
+                </ul>
+            </div>
+            <div class="popup-user-details">
+                <p>whatever</p>
+            </div>
+        </div>
+    </div>`
+    );
+}
+
+function fetchUsersAndGenerateHtml(lastUserIndex){
+    var url="https://api.github.com/users"
+
+    if(lastUserIndex == undefined){
+        return console.log("Last fetched user is undefined");
+    }
+    else if(lastUserIndex != 0){
+        url = url + "?since=" + lastUserIndex;
+    }
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(data, status, jqXHR){
+            console.log(data);
+            console.log(status);
+            console.log(jqXHR);
+
+            var htmlUserItems="";
+            data.forEach(element => {
+                var userItem = `<li class="user-selected-js">
+                                    <a href="empty" id="`+ element.id +`">`+ element.login +`</a>
+                                </li>`;
+                htmlUserItems = htmlUserItems + userItem;
+            });
+            
+            $(".users-list").append(htmlUserItems).fadeIn();
+        }
+    });
 }
 
 function fetchSingleUser() {
