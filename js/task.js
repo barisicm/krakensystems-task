98a59fbc9d055315a51f1451c04640e13722e965
Marin Barišić <barisic.m@outlook.com>
2018-01-24 16:33:56 +0100

    Finished product. TODO::Testing

--- js/task.js
+++ js/task.js
@@ -1,70 +1,18 @@
-function initialize() {
-
-    //Initialise popup
-    generatePopupHtml($("#preloader"));
-    fetchUsers(0);
-
-    //Click Listeners
-    var popupActive=false;
-    $(".popup-header-container").click(function(){
-        if(!popupActive){
-            $(".github-users").animate({bottom: "350px"},400, null);
-            popupActive=true;
-        } else {
-            $(".github-users").animate({bottom: "50px"},400, null);
-            popupActive=false;
-        }
-    });
-
-    $(".user-link-js").click(function(){
-        fetchUser(this.text());
-    });
-
-    //Scroll Listeners
-    var popupList = $('.users-list');
-    popupList.scroll(function () {
-
-        var scrollTop = $(this).scrollTop();
-        var innerHeight = $(this).innerHeight();
-        var scrollHeight = $(this)[0].scrollHeight;
-
-        if(scrollTop + innerHeight >= scrollHeight - 1) {
-            var lastUser = $('.user-selected-js > a').length;
-            fetchUsers(lastUser);
-            $(this).scrollTop(scrollHeight - innerHeight);
-        }
-    });
-}
-
 function generatePopupHtml(targetElement){
+    //Generates and appends popup html
     $(targetElement).after(
     `<div id="users" class="row-full-width">
         <div class="github-users">
             <div class="popup-header-container">
                 <h3>GitHub Users</h3>
+                <i class="fa fa-remove hidden close-user-js"></i>
             </div>
-            <div class="popup-list-container" style="display:none;">
+            <div class="popup-list-container">
                 <ul class="users-list">
                 </ul>
             </div>
-            <div class="popup-user-details" style="display:block;">
+            <div class="popup-user-details">
                 <div class="user-details-inner-container">
-                    <div class="row">
-                        </br>
-                        <img class="user-image" src="https://avatars0.githubusercontent.com/u/1?v=4"></img>
-                        <h3 style="margin:0;">Stats</h3>
-                        </br>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <p>Public repos : </p>
-                            <p>repos</p>
-                        </div>
-                        <div class="float-right col-md-6">
-                            <p>Public gists : </p>
-                            <p>gists</p>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -73,6 +21,7 @@ function generatePopupHtml(targetElement){
 }
 
 function fetchUsers(lastUserIndex){
+    //Fetches users from the last user fetched
     var url="https://api.github.com/users"
 
     if(lastUserIndex == undefined){
@@ -92,17 +41,45 @@ function fetchUsers(lastUserIndex){
 }
 
 function appendUsersToList(data, targetElement){
+    //Filters seen users from fetched users and append
+    var savedUsers = getUsersFromLocalStorage();
+    var hasUserBeenSeen = false;
     var htmlUserItems="";
 
-    data.forEach(element => 
-    {
-        var userItem = `
-        <li class="user-selected-js">
-            <a href="" class="user-link-js" >`+ element.login +`</a>
-        </li>`;
-        htmlUserItems = htmlUserItems + userItem;
-    });
-            
+    if(savedUsers==null || savedUsers==undefined){
+        data.forEach(element => 
+        {
+            var userItem = `
+                <li class="user-selected-js">
+                    <a href="" class="user-link-js" >`+ element.login +`</a>
+                </li>`;
+                htmlUserItems += userItem;
+        });
+    } else {
+        for(var j=0;j<data.length;j++) 
+        {
+            for(var i=0; i<savedUsers.length; i++){
+                if(savedUsers[i] == data[j].login){
+                    hasUserBeenSeen=true;
+                    break;
+                }
+                else {
+                    hasUserBeenSeen=false;
+                }
+            }
+            if(hasUserBeenSeen){
+                continue;
+            }
+            else{
+                var userItem = `
+                <li class="user-selected-js">
+                    <a href="" class="user-link-js" >`+ data[j].login +`</a>
+                </li>`;
+                htmlUserItems += userItem;
+            }
+        };
+    }
+
     $(targetElement).append(htmlUserItems).fadeIn();
 }
 
@@ -113,13 +90,103 @@ function fetchUser(username) {
         url: url,
         method: "GET",
         success: function(data, status, jqXHR){
-            showUserDetails(data);
+            showUserDetails(data, ".user-details-inner-container");
         }
     })
 }
 
-function showUserDetails(data){
+function showUserDetails(data, targetElement){
+    var company = data.company;
+    if(company==null){
+        company="None"
+    }
 
+    var htmlUserDetails = `                    
+    <div class="row">
+        </br>
+        <img class="user-image" src=`+ data.avatar_url +`></img>
+        <h3 class="no-margin">Stats</h3>
+        <span>Company: `+ company +`</span>
+        </br>
+    </div>
+    <div class="row sm-margin-bottom">
+        <div class="col-md-6">
+            <p>Public repos : </p>
+            <p>` + data.public_repos + `</p>
+        </div>
+        <div class="float-right col-md-6">
+            <p>Public gists : </p>
+            <p>` + data.public_gists + `</p>
+        </div>
+    </div>`
+    $(targetElement).append(htmlUserDetails);
+    $(".popup-header-container > h3").text(data.login);
 }
 
-$(document).ready(initialize());
+var viewedUsers = [];
+function saveUsersToLocalStorage(username){
+    viewedUsers.push(username);
+    localStorage.setItem("viewedUsers", JSON.stringify(viewedUsers));
+}
+
+function getUsersFromLocalStorage(){
+    return JSON.parse(localStorage.getItem("viewedUsers"));
+}
+
+$(document).ready(function(){
+    //Initialise popup
+    generatePopupHtml($("#preloader"));
+    fetchUsers(0);
+ 
+    //Click Listeners
+    var popupActive=false;
+    $(".popup-header-container").click(function(){
+        //Show/hide popup
+        if(!popupActive){
+            $(".github-users").animate({bottom: "350px"},400, null);
+            popupActive=true;
+        } else {
+            $(".github-users").animate({bottom: "50px"},400, null);
+            popupActive=false;
+        }
+    });
+
+    $(".popup-header-container").find("i").click(function(event){
+        //Close user details
+        event.stopPropagation();
+        $(".close-user-js").addClass("hidden");
+        fetchUsers(0);
+        $(".popup-list-container").show();
+        $(".popup-list-container").animate({opacity: '1'},1000,null);
+        $(".popup-header-container > h3").text("GitHub Users");
+        $(".user-details-inner-container").empty();
+         
+    });
+
+    $(document).on('click','.user-link-js',function(e){
+        //Open user details
+        e.preventDefault();
+        saveUsersToLocalStorage($(this).text());
+        fetchUser($(this).text());
+        $(".popup-list-container").animate({opacity: '0'},1000,function(){
+            $(this).hide();
+            $(".close-user-js").removeClass("hidden");
+            $(".users-list").empty();
+        });
+    });
+ 
+    //Scroll Listeners
+    var popupList = $('.users-list');
+    popupList.scroll(function () {
+        //Load more users
+        var scrollTop = $(this).scrollTop();
+        var innerHeight = $(this).innerHeight();
+        var scrollHeight = $(this)[0].scrollHeight;
+ 
+        if(scrollTop + innerHeight >= scrollHeight - 1) {
+            var lastUser = $('.user-selected-js > a').length;
+            fetchUsers(lastUser);
+            $(this).scrollTop(scrollHeight - innerHeight);
+        }
+    });
+});
