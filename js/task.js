370d691934bd1bc87954ac7ff4f153f13f8801c8
Marin Barišić <barisic.m@outlook.com>
2018-01-25 01:15:33 +0100

    Commit

--- js/task.js
+++ js/task.js
@@ -1,5 +1,60 @@
+$(document).ready(function(){
+    //Initialise popup
+    generatePopupHtml($("#preloader"));
+    fetchUsers(0);
+ 
+    //Click Listeners
+    var popupActive=false;
+    $(".popup-header-container").click(function(){
+        //Show/hide popup
+        if(!popupActive){
+            $(".github-users").animate({bottom: "350px"},400, null);
+            popupActive=true;
+        } else {
+            $(".github-users").animate({bottom: "50px"},400, null);
+            popupActive=false;
+        }
+    });
+
+    $(".popup-header-container").find(".close-user-js").click(function(event){
+        //Close user details
+        event.stopPropagation();
+        $(".close-user-js").addClass("hidden");
+        $(".popup-list-container").show().animate({opacity: '1'},500,null);
+        $(".popup-header-container > h3").text("GitHub Users");
+        $(".user-details-inner-container").empty();
+        removeSeenUsersFromList();
+    });
+
+    $(document).on('click','.user-link-js',function(e){
+        //Open user details
+        e.preventDefault();
+        saveUsersToLocalStorage($(this).text());
+        fetchUser($(this).text());
+        $(".popup-list-container").animate({opacity: '0'},500,function(){
+            $(this).hide();
+            $(".close-user-js").removeClass("hidden");
+        });
+    });
+ 
+    //Scroll Listeners
+    var popupList = $('.users-list');
+    popupList.scroll(function () {
+        
+        var scrollTop = $(this).scrollTop();
+        var innerHeight = $(this).innerHeight();
+        var scrollHeight = $(this)[0].scrollHeight;
+        
+        //Load more users
+        if(scrollTop + innerHeight >= scrollHeight - 1) {
+            var lastUser = $('.user-selected-js').length;
+            fetchUsers(lastUser);
+            $(this).scrollTop(scrollHeight - innerHeight);
+        }
+    });
+});
+
 function generatePopupHtml(targetElement){
-    //Generates and appends popup html
     $(targetElement).after(
     `<div id="users" class="row-full-width">
         <div class="github-users">
@@ -18,81 +73,6 @@ function generatePopupHtml(targetElement){
         </div>
     </div>`
     );
-}
-
-function fetchUsers(lastUserIndex){
-    //Fetches users from the last user fetched
-    var url="https://api.github.com/users"
-
-    if(lastUserIndex == undefined){
-        return console.log("Last fetched user is undefined");
-    }
-    else if(lastUserIndex != 0){
-        url = url + "?since=" + lastUserIndex;
-    }
-
-    $.ajax({
-        url: url,
-        method: "GET",
-        success: function(data, status, jqXHR){
-            appendUsersToList(data,".users-list");
-        }
-    });
-}
-
-function appendUsersToList(data, targetElement){
-    //Filters seen users from fetched users and append
-    var savedUsers = getUsersFromLocalStorage();
-    var hasUserBeenSeen = false;
-    var htmlUserItems="";
-
-    if(savedUsers==null || savedUsers==undefined){
-        data.forEach(element => 
-        {
-            var userItem = `
-                <li class="user-selected-js">
-                    <a href="" class="user-link-js" >`+ element.login +`</a>
-                </li>`;
-                htmlUserItems += userItem;
-        });
-    } else {
-        for(var j=0;j<data.length;j++) 
-        {
-            for(var i=0; i<savedUsers.length; i++){
-                if(savedUsers[i] == data[j].login){
-                    hasUserBeenSeen=true;
-                    break;
-                }
-                else {
-                    hasUserBeenSeen=false;
-                }
-            }
-            if(hasUserBeenSeen){
-                continue;
-            }
-            else{
-                var userItem = `
-                <li class="user-selected-js">
-                    <a href="" class="user-link-js" >`+ data[j].login +`</a>
-                </li>`;
-                htmlUserItems += userItem;
-            }
-        };
-    }
-
-    $(targetElement).append(htmlUserItems).fadeIn();
-}
-
-function fetchUser(username) {
-    var url = "https://api.github.com/users/" + username;
-
-    $.ajax({
-        url: url,
-        method: "GET",
-        success: function(data, status, jqXHR){
-            showUserDetails(data, ".user-details-inner-container");
-        }
-    })
 }
 
 function showUserDetails(data, targetElement){
@@ -123,9 +103,70 @@ function showUserDetails(data, targetElement){
     $(".popup-header-container > h3").text(data.login);
 }
 
-var viewedUsers = [];
+function appendUsersToList(data, targetElement){
+    var htmlUserItems="";
+    data.forEach(element => 
+    {
+        var userItem = `
+            <li class="user-selected-js">
+                <a href="" class="user-link-js" >`+ element.login +`</a>
+            </li>`;
+            htmlUserItems += userItem;
+    });
+
+    $(targetElement).append(htmlUserItems).fadeIn();
+}
+
+function fetchUsers(lastUserIndex){
+    var url="https://api.github.com/users"
+
+    if(lastUserIndex == undefined){
+        return console.log("Last fetched user is undefined");
+    }
+    else if(lastUserIndex != 0){
+        url += "?since=" + lastUserIndex;
+    }
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(data, status, jqXHR){
+            appendUsersToList(data,".users-list");
+        }
+    });
+}
+
+function fetchUser(username) {
+    var url = "https://api.github.com/users/" + username;
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(data, status, jqXHR){
+            showUserDetails(data, ".user-details-inner-container");
+        }
+    })
+}
+
+function removeSeenUsersFromList(){
+    var seenUsers = getUsersFromLocalStorage();
+    if(seenUsers != null && seenUsers != undefined){
+        for(var i = 0; i<seenUsers.length; i++){
+            $(".user-selected-js > .user-link-js:contains('"+ seenUsers[i] +"')" ).parent().remove();
+        }
+    }
+}
+
 function saveUsersToLocalStorage(username){
-    viewedUsers.push(username);
+    var viewedUsers = getUsersFromLocalStorage();
+
+    if(viewedUsers != null && viewedUsers != undefined){
+        viewedUsers.push(username);
+    }
+    else{
+        viewedUsers = [];
+        viewedUsers.push(username);
+    }
     localStorage.setItem("viewedUsers", JSON.stringify(viewedUsers));
 }
 
@@ -133,60 +174,5 @@ function getUsersFromLocalStorage(){
     return JSON.parse(localStorage.getItem("viewedUsers"));
 }
 
-$(document).ready(function(){
-    //Initialise popup
-    generatePopupHtml($("#preloader"));
-    fetchUsers(0);
- 
-    //Click Listeners
-    var popupActive=false;
-    $(".popup-header-container").click(function(){
-        //Show/hide popup
-        if(!popupActive){
-            $(".github-users").animate({bottom: "350px"},400, null);
-            popupActive=true;
-        } else {
-            $(".github-users").animate({bottom: "50px"},400, null);
-            popupActive=false;
-        }
-    });
 
-    $(".popup-header-container").find("i").click(function(event){
-        //Close user details
-        event.stopPropagation();
-        $(".close-user-js").addClass("hidden");
-        fetchUsers(0);
-        $(".popup-list-container").show();
-        $(".popup-list-container").animate({opacity: '1'},1000,null);
-        $(".popup-header-container > h3").text("GitHub Users");
-        $(".user-details-inner-container").empty();
-         
-    });
 
-    $(document).on('click','.user-link-js',function(e){
-        //Open user details
-        e.preventDefault();
-        saveUsersToLocalStorage($(this).text());
-        fetchUser($(this).text());
-        $(".popup-list-container").animate({opacity: '0'},1000,function(){
-            $(this).hide();
-            $(".close-user-js").removeClass("hidden");
-            $(".users-list").empty();
-        });
-    });
- 
-    //Scroll Listeners
-    var popupList = $('.users-list');
-    popupList.scroll(function () {
-        //Load more users
-        var scrollTop = $(this).scrollTop();
-        var innerHeight = $(this).innerHeight();
-        var scrollHeight = $(this)[0].scrollHeight;
- 
-        if(scrollTop + innerHeight >= scrollHeight - 1) {
-            var lastUser = $('.user-selected-js > a').length;
-            fetchUsers(lastUser);
-            $(this).scrollTop(scrollHeight - innerHeight);
-        }
-    });
-});
